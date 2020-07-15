37acbedc4406c98bd2faba1a21b0029aec99bab2
iTranscend <llordbanks500@gmail.com>
2020-07-15 23:50:39 +0100

    Refactor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
+const path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(__dirname + "/public"));
 
 app.use(
   session({
