ce357e2d7c858b8434919e40032a0706d549cee6
iTranscend <llordbanks500@gmail.com>
2020-07-15 23:48:53 +0100

    Static files for UI

---
+++ public/script.js
@@ -0,0 +1,13 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+            $('#profile-img-tag').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#profile-img").change(function(){
+    readURL(this);
+});
