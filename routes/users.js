b4ebd50249335111b15a28f59f128767c77ad761
iTranscend <llordbanks500@gmail.com>
2020-07-16 21:13:37 +0100

    Implemented results module for users

--- routes/users.js
+++ routes/users.js
@@ -27,40 +27,51 @@ router.get("/home", async function (req, res, next) {
         if (error) throw error;
         await console.log(results[0].firstname);
 
-        conn.query(
-          "SELECT id, name FROM positions ORDER BY id",
-          async function (err, positions, fields) {
-            let candidacy = [];
-            if (err) throw err;
-            await console.log("Positions:", positions);
-            await positions.forEach(async (element) => {
-              conn.query(
-                "SELECT u.firstname AS firstname, u.lastname AS lastname, u.profile_pic AS profile_pic, c.id AS candidate_id FROM users u JOIN candidates c ON u.id = c.user_id JOIN positions p ON p.id = c.position_id WHERE c.is_approved = 1 and p.id = ? ORDER BY p.id",
-                element.id,
-                async function (error, candidates, fields) {
-                  if (error) throw error;
-                  // await console.log(candidates);
-                  // Formation of the compound object to be sent back to user
-                  candidacy[element.name] = candidates;
-                  candidacy[element.name] = {
-                    id: element.id,
-                    candidates,
-                  };
-                }
-              );
-            });
-            await setTimeout(async () => {
-              await console.log("Candidacy:", candidacy);
-              return res.render("user/home", {
-                title: "Elections",
-                message: req.session.message,
-                user: results[0],
-                candidacy,
+        // Check if results have been published
+        conn.query("SELECT * from config WHERE id = 2", function (
+          err,
+          published,
+          fields
+        ) {
+          let isPublished = published[0].value;
+          console.log("Results Published: " + isPublished);
+          // Get positions of office
+          conn.query(
+            "SELECT id, name FROM positions ORDER BY id",
+            async function (err, positions, fields) {
+              let candidacy = [];
+              if (err) throw err;
+              await console.log("Positions:", positions);
+              await positions.forEach(async (element) => {
+                conn.query(
+                  "SELECT u.firstname AS firstname, u.lastname AS lastname, u.profile_pic AS profile_pic, c.id AS candidate_id FROM users u JOIN candidates c ON u.id = c.user_id JOIN positions p ON p.id = c.position_id WHERE c.is_approved = 1 and p.id = ? ORDER BY p.id",
+                  element.id,
+                  async function (error, candidates, fields) {
+                    if (error) throw error;
+                    // await console.log(candidates);
+                    // Formation of the compound object to be sent back to user
+                    candidacy[element.name] = candidates;
+                    candidacy[element.name] = {
+                      id: element.id,
+                      candidates,
+                    };
+                  }
+                );
               });
-              // req.session.message = "";
-            }, 4000);
-          }
-        );
+              await setTimeout(async () => {
+                await console.log("Candidacy:", candidacy);
+                return res.render("user/home", {
+                  title: "Elections",
+                  message: req.session.message,
+                  user: results[0],
+                  candidacy,
+                  isPublished,
+                });
+                // req.session.message = "";
+              }, 2500);
+            }
+          );
+        });
       }
     );
   }
@@ -233,6 +244,87 @@ router.post("/apply/:id", upload.none(), async function (req, res, next) {
     );
   } catch (error) {
     console.log(error);
+  }
+});
+
+// Route to results page
+router.get("/results", function (req, res, next) {
+  if (!req.session.email) {
+    res.redirect("/");
+  } else {
+    // Check if results have been published
+    conn.query("SELECT * from config WHERE id = 2", function (
+      err,
+      published,
+      fields
+    ) {
+      let isPublished = published[0].value;
+      console.log("Results Published: " + isPublished);
+      if (isPublished == 0) {
+        req.session.message = "The results have not yet been published";
+        res.redirect("/users/home");
+      } else if (isPublished == 1) {
+        conn.query(
+          "SELECT * FROM users WHERE email = ?",
+          req.session.email,
+          function (error, results, fields) {
+            conn.query(
+              "SELECT id, name FROM positions ORDER BY id",
+              async function (err, positions, fields) {
+                let candidacy = [];
+                if (err) throw err;
+                await console.log("Positions:", positions);
+                await positions.forEach(async (position) => {
+                  conn.query(
+                    "SELECT u.firstname AS firstname, u.lastname AS lastname, u.profile_pic AS profile_pic, c.id AS candidate_id FROM users u JOIN candidates c ON u.id = c.user_id JOIN positions p ON p.id = c.position_id WHERE c.is_approved = 1 and p.id = ? ORDER BY p.id",
+                    position.id,
+                    async function (error, candidates, fields) {
+                      if (error) throw error;
+                      // candidacy[position.name];
+
+                      await candidates.forEach((candidate) => {
+                        console.log(candidate);
+                        conn.query(
+                          "SELECT COUNT(*) AS count FROM votes WHERE position_id = ? AND candidate_id = ?",
+                          [position.id, candidate.candidate_id],
+                          async function (error, value, fields) {
+                            let voteCount = await value[0].count;
+                            await console.log(
+                              position.id +
+                                " : " +
+                                candidate.candidate_id +
+                                " : " +
+                                voteCount
+                            );
+                            candidate.votes = await voteCount;
+                          }
+                        );
+                      });
+
+                      candidacy[position.name] = {
+                        id: position.id,
+                        candidates,
+                      };
+                    }
+                  );
+                });
+                await setTimeout(async () => {
+                  await console.log("Candidacy:", candidacy);
+                  req.session.message = "";
+                  return res.render("user/results", {
+                    title: "Elections",
+                    message: req.session.message,
+                    user: results[0],
+                    candidacy,
+                  });
+                  // req.session.message = "";
+                }, 2500);
+              }
+            );
+          }
+        );
+      }
+    });
   }
 });
 
